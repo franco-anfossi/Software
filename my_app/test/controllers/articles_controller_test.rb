6d64958a839e8764ea3c9e9cf1c2206a28874a1c
Franco Anfossi <franco.anfossi@uc.cl>
2023-04-14 14:59:07 -0400

    archivos por c12 y testing

--- my_app/test/controllers/articles_controller_test.rb
+++ my_app/test/controllers/articles_controller_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+  
+  teardown do
+    Rails.cache.clear
+  end
+  
+  test "should get index" do
+    get articles_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get new_article_url
+    assert_response :success
+  end
+
+  # crea objeto y lo guarda y despues envia request y verifica que sea success
+  test "should show article" do
+    @article = Article.new(title: "Creating an article by test", body: "This is a body")
+    @article.save
+
+    get "/articles/#{@article.id}"
+    assert_response :success
+  end
+  # Son lo mismo
+  test "should show article" do
+    @article = Article.new(title: "Creating an article by test", body: "This is a body")
+    @article.save
+
+    get article_url(@article)
+    assert_response :success
+  end
+  
+  test "should create article" do
+    assert_difference("Article.count") do
+      post articles_url, params: {
+        article: {
+          title: "Creating an article by test",
+          body: "This is a body"
+        }
+      }
+    end
+    assert_redirected_to article_path(Article.last)
+  end
+
+  test "should destroy article" do
+    @article = Article.new(title: "Other article by test", body: "Delete me")
+    @article.save
+
+    assert_difference("Article.count", -1) do
+      delete article_url(@article)
+    end
+
+    assert_redirected_to articles_url
+  end
 end
--- my_app/test/models/article_test.rb
+++ my_app/test/models/article_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save article without title" do
+    # Inicializacion, se crean los objetos
+    article = Article.nes
+    # Estimulo, donde se realiza la accion a testear
+    result = article.save
+    # Verificacion, donde se evalua si el resultado es el esperado
+    assert_not result, "Saved an article without a title"
+  end
+
+  #test "should not save article without title" do
+    #article = @article(:article1)
+    #result = @article.save
+    #assert_not result, "Saved an article without a title"
+  #end
 end
